3267a41b04eadbff3c2681bc38ce698660141003
ronin <sobirjonismatov71@gmail.com>
2025-05-15 16:23:49 +0500

    .

--- src/s21_strncmp.c
+++ src/s21_strncmp.c
@@ -1,5 +1,5 @@
-#include "s21_string.h"
+// #include "s21_string.h"
 
-int s21_strncmp(const char *str1, const char *str2, s21_size_t n){
-        
-}
+// int s21_strncmp(const char *str1, const char *str2, s21_size_t n){
+
+// }
