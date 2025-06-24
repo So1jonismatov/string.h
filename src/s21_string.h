8df7428f8efafdcdd8ad5edab3b8fdcb49595b89
Sobirjon Ismatov <118159969+So1jonismatov@users.noreply.github.com>
2025-06-24 17:45:45 +0500

    final

---
+++ src/s21_string.h
@@ -0,0 +1,26 @@
+#ifndef _S21_STRING
+#define _S21_STRING
+
+
+typedef unsigned long int s21_size_t;
+#define S21_NULL ((void*)0)
+
+
+void *s21_memchr(const void *str, int c, s21_size_t n);
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
+void *s21_memcpy(void *dest, const void *src, s21_size_t n);
+void *s21_memset(void *str, int c, s21_size_t n);
+s21_size_t s21_strlen(const char *str); // uzunlik
+char *s21_strncat(char *dest, const char *src, s21_size_t n);
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
+char *s21_strchr(const char *str, int c);
+char *s21_strncpy(char *dest, const char *src, s21_size_t n);
+s21_size_t s21_strcspn(const char *str1, const char *str2);
+char *s21_strpbrk(const char *str1, const char *str2);
+char *s21_strrchr(const char *str, int c);
+s21_size_t s21_strspn(const char *str1, const char *str2);
+char * s21_strstr(const char * haystack, const char * needle);
+char *s21_strtok(char *str, const char *delim);
+char *s21_strcpy(char *dest, const char *src);
+
+#endif
---
+++ src/s21_strncat.c
@@ -0,0 +1,18 @@
+#include "s21_string.h"
+
+char *s21_strncat(char *result, const char *str, s21_size_t size) {
+  int strLength = s21_strlen(str);
+
+  for (s21_size_t x = 0; x < size && str[x] != '\0'; x += 1) {
+    result[strLength + x] = str[x];
+    result[strLength + x + 1] = '\0';
+  }
+
+  for (s21_size_t x = 0, strLength = s21_strlen(str);
+       x < size && str[x] != '\0'; x += 1) {
+    result[strLength + x] = str[x];
+    result[strLength + x + 1] = '\0';
+  }
+
+  return result;
+}
